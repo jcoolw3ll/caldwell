34b748c3f30f3fc33e63541e4c7ee8554edece03
jcoolw3ll <jacob.caldwell@students.mq.edu.au>
2025-06-08 00:45:02 +1000

    UNITY part finished

--- COMP1151 Game Prototype/Assets/Tiled/Dreamride Tilesheet.tsx
+++ COMP1151 Game Prototype/Assets/Tiled/Dreamride Tilesheet.tsx
@@ -1,4 +1,34 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="Dreamride Tilesheet" tilewidth="16" tileheight="16" tilecount="1036" columns="37">
  <image source="Tilemap/kenney_roguelike-modern-city/Tilemap/tilemap_packed.png" width="592" height="448"/>
+ <tile id="686">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="0" width="2.18182" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="690">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="12.8182" y="0" width="2.45455" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="794">
+  <objectgroup draworder="index" id="5">
+   <object id="13" x="13.9091" y="0" width="2" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="795">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="2.18182" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="972">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="2.27273" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="974">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13.5455" y="0" width="2.45455" height="16"/>
+  </objectgroup>
+ </tile>
 </tileset>
